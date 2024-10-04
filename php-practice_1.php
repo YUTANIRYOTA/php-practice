037e9713ecfff341a8277ec5553894a5e9a88de3
YUTANIRYOTA <nico0515d@gmail.com>
2024-10-04 15:16:53 +0900

    課題１の修正

--- php-practice_1.php
+++ php-practice_1.php
@@ -8,7 +8,15 @@ $name = '油谷';
 echo "私の名前は「 $name 」です。";
 
 ?>
+変数には何が格納できますか？
+値
 
+また変数に格納することでどのようなメリットがあるか教えてください！
+同じデータを利用できる。新しい値を格納することでデータの入れ替えができる。
+
+シングルとダブルの違いはなにかありますか？
+シングルは、左辺の変数に右辺の値を代入するという意味
+ダブルは、等しいという意味
 // Q2 四則演算
 <?php
 
@@ -21,6 +29,8 @@ var_dump($num);
 var_dump($num / 2)
 
 ?>
+変数xとyに格納している値のデータ型を教えてください！
+データ型はintです。
 
 Q3// Q3 日付操作
 
@@ -33,6 +43,9 @@ $date = new DateTime('now');
 echo $date->format('現在時刻は、Y年m月d日 H時i分s秒です。');
 
 ?>
+
+date_default_timezone_set('Asia/Tokyo');この記述はなぜ必要ですか？
+タイムゾーンを設定しないと正しい時刻が表示されないと考えていましたが、無くても変わらないので必要なかったです。
 
 // Q4 条件分岐-1 if文
 
@@ -48,7 +61,11 @@ else {
     echo "どちらでもありません。"; 
 }
 ?>
+「===」と「==」で比較する違いを教えて下さい！
+＝＝＝は、データ型まで完全一致。＝＝は、値の一致。
 
+if文の条件ですが$device === "mac" || $device === "windows"の後に他にも条件を追加することはできますか？
+論理演算子を使用し、複数の条件式を組み合わせることが可能。よって条件の追加は可能です。
 // Q5 条件分岐-2 三項演算子
 
 <?php
@@ -60,6 +77,8 @@ echo $message;
 
 ?>
 
+条件分岐はif文でも表現できますが三項演算子を使うメリットはなにかありますか！
+分岐が2つで変数代入の処理が1つのみの場合に便利です。余分な変数宣言や代入がないため、読みやすいという特徴がある。
 // Q6 配列
 
 <?php
@@ -69,6 +88,11 @@ echo "$prefectures[3]と$prefectures[4]は関東地方の都道府県です。";
 
 ?>
 
+配列には文字列しか格納できないですか？
+文字列以外のデータの格納も可能です。
+
+栃木県と千葉県は配列の要素で数えると4番目と5番目ですが、なぜ3と4番目を取得するのでしょうか？
+インデックス番号は0から始まるため、3と4番目になります。
 // Q7 連想配列-1
 
 <?php
@@ -82,19 +106,24 @@ $todouhuken = [
     '茨城県' => '水戸市'
 ];
 
-foreach ($todouhuken as $x => $y) {
-    echo "$y"."\n";
+foreach ($todouhuken as $kenmei => $kenchou) {
+    echo "$kenchou\n";
 }
 ?>
 
+配列と連想配列には違いがありますか？あれば教えて下さい！
+違いは、連想配列は要素にラベルを付けられること。
+
+ダブルクォーテーションであれば\nを結合する必要ありますか？
+必要なし
 // Q8 連想配列-2
 
 <?php
 
 $ken = '埼玉県';
-foreach($todouhuken as $x => $y){
-    if($x == $ken){
-echo "$x の県庁所在地は、$y です。\n";
+foreach($todouhuken as $kenmei => $kenchou){
+    if($kenmei == $ken){
+echo "$kenmei の県庁所在地は、$kenchou です。\n";
 }
 }
 
@@ -105,11 +134,11 @@ echo "$x の県庁所在地は、$y です。\n";
 <?php
 
 $todouhuken += ['愛知県' => '名古屋市','大阪府' => '大阪'];
-foreach($todouhuken as $x => $y){
-    if($x == '大阪府' || $x == '愛知県' ){
-echo "$x は、関東地方ではありません。\n";
+foreach($todouhuken as $kenmei => $kenchou){
+    if($kenmei == '大阪府' || $kenmei == '愛知県' ){
+echo "$kenmei は、関東地方ではありません。\n";
 }else{
-    echo "$x の県庁所在地は、$y です。\n";
+    echo "$kenmei の県庁所在地は、$kenchou です。\n";
 }
 }
 ?>
@@ -128,18 +157,24 @@ sayHi("金谷");
 sayHi("安藤");
 ?>
 
+関数は定義しただけで実行できますか？
+実行できない。呼び出す必要がある。
+引数に設定している金谷や安藤はどこで受け取っていますか？
+
+function sayHi($name) で受け取る
 // Q11 関数-2
 
 
 <?php
+
 $price = 1000;
 function calcTaxInPrice($price)
 {
     $taxInPrice = $price * 1.1;
-    echo $price . "円の、商品の税込価格は $taxInPrice 円です。";
+    return  $price . "円の、商品の税込価格は" . $taxInPrice . "円です。";
 }
-
-calcTaxInPrice($price);
+$taxInPrice = calcTaxInPrice($price);
+echo $taxInPrice;
 
 ?>
 
@@ -148,17 +183,19 @@ calcTaxInPrice($price);
 
 <?php
 
-function distinguishNum($n)
-{
-if($n % 2 === 0){
-echo $n . "は偶数です\n";
+function distinguishNum($n){
+    
+ if($n % 2 === 0){
+  return $n . "は偶数です\n";
+ }
 
-}else{
-echo $n . "は奇数です\n";
+ else{
+  return $n . "は奇数です\n";
+ }
 }
-}
-distinguishNum(11);
-distinguishNum(24);
+
+echo distinguishNum(11);
+echo distinguishNum(24);
 ?>
 
 // Q13 関数とswitch文
@@ -171,24 +208,24 @@ function evaluateGrade($signal)
 switch ($signal) {
     case "A":
     case "B";
-        echo "合格です。\n";
+        return "合格です。\n";
         break;
 
     case "C":
-        echo "合格ですが追加課題があります。\n";
+        return "合格ですが追加課題があります。\n";
         break;
 
     case "D":
-        echo "不合格です。\n";
+        return "不合格です。\n";
         break;
 
     default:
-        echo '判定不明です。講師に問い合わせてください。';
+        return '判定不明です。講師に問い合わせてください。';
         break;
 }
 }
-evaluateGrade("A");
-evaluateGrade("E");
+echo evaluateGrade("A");
+echo evaluateGrade("E");
 ?>
 
 
